66faeef93208961096563b2be19bc31873ed8c0c
Jesusjbs <jesusbarba11@gmail.com>
2020-03-15 13:33:42 +0100

    Enter

---
+++ Taller EuroTyre/error.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>error</title>
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="./img/logo.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	</head>
+
+	<body style="background-color: #4EB5ED;">
+		<header>
+			<h1 align="center" style="color: #FFFFFF;padding: 5px;">Ups! Parece que la página que has solicitado aún no se encuentra disponible</h1>
+		</header>
+		<img style="width:800px;height:600px;display: block;margin: auto;background-image:url('img/404.gif'); background-color: #4EB5ED;">
+	</body>
+</html>
