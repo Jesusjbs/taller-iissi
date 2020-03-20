87db54cdae571ef5cf6c01d2ca4ac7f40044fa74
Jesusjbs <jesusbarba11@gmail.com>
2020-03-20 11:21:30 +0100

    actualizado

--- Taller EuroTyre/cabecera.php
+++ Taller EuroTyre/cabecera.php
@@ -1,19 +1,29 @@
-<header>
-    <img id="id_logo" src="./img/logo.png" height="125px" alt="Logo EuroTyre" />
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/style_cabecera.css" />
+    </head>
+    <bodi>
+        <section id="id_superior">
+            <header>
+                <img id="id_logo" src="./img/logo.png" height="125px" alt="Logo EuroTyre" />
 
-    <nav class="menu">
-        <ul>
-            <li><a href="#">Nosotros</a></li>
-            <li><a href="#">F.A.Q</a></li>
-            <li><a href="#">Acerca</a></li>
-        </ul>
-    </nav>
+                <nav class="menu">
+                    <ul>
+                        <li><a href="#">Nosotros</a></li>
+                        <li><a href="#">F.A.Q</a></li>
+                        <li><a href="#">Acerca</a></li>
+                    </ul>
+                </nav>
 
-    <nav class="menu2">
-        <ul>
-            <li><a href="#">Solicitar cita</a></li>
-            <li><a href="#">Consulta</a></li>
-            <li><a href="#">Mis Vehículos</a></li>
-        </ul>
-    </nav>
-</header>
+                <nav class="menu2">
+                    <ul>
+                        <li><a href="#">Solicitar cita</a></li>
+                        <li><a href="#">Consulta</a></li>
+                        <li><a href="#">Mis Vehículos</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </section>
+    </bodi>
+</html>
